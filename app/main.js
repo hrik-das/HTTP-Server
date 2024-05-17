73fbcac2aaa8b56f681be8d73b2b863840c1c151
hrik das <emptynull01@gmail.com>
2024-05-17 18:17:01 +0530

    Update main.js

--- app/main.js
+++ app/main.js
@@ -8,62 +8,58 @@ console.log("Logs from your program will appear here!");
 let directory = ".";
 const logFileName = "server.log";
 const directoryIndex = process.argv.indexOf("--directory");
-if (directoryIndex !== -1) {
+if(directoryIndex !== -1)
     directory = process.argv[directoryIndex + 1];
-}
 
 const handleConnection = (socket) => {
     socket.on("data", (data) => {
         const [request, host, agent] = data.toString().split("\r\n");
         const [method, path, version] = request.split(" ");
         logToFile(`[${new Date().toISOString()}] Request: ${method} ${path} ${version}`);
-        switch (method) {
-            case "GET":
-                handleGetRequest(socket, path, agent);
-                break;
-            case "POST":
-                handlePostRequest(socket, path, data);
-                break;
-            default:
-                sendResponse(socket, "HTTP/1.1 405 Method Not Allowed\r\n\r\n");
-                socket.end();
-                break;
+        switch(method){
+            case "GET": handleGetRequest(socket, path, agent);
+                        break;
+            case "POST": handlePostRequest(socket, path, data);
+                         break;
+            default: sendResponse(socket, "HTTP/1.1 405 Method Not Allowed\r\n\r\n");
+                     socket.end();
+                     break;
         }
     });
 };
 
 const handleGetRequest = (socket, path, agent) => {
-    if (path === "/") {
+    if(path === "/"){
         sendResponse(socket, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\nHello, World!");
-    } else if (path.startsWith("/echo/")) {
+    }else if(path.startsWith("/echo/")){
         const pathParameter = path.replace("/echo/", "");
         sendResponse(socket, `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${pathParameter.length}\r\n\r\n${pathParameter}`);
-    } else if (path.toLowerCase().startsWith("/user-agent")) {
+    }else if(path.toLowerCase().startsWith("/user-agent")){
         const userAgent = agent.replace("User-Agent: ", "");
         sendResponse(socket, `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
-    } else if (path.startsWith("/files/")) {
+    }else if(path.startsWith("/files/")){
         const filePath = directory + "/" + path.slice(7);
-        if (!fs.existsSync(filePath) || !fs.statSync(filePath).isFile()) {
+        if(!fs.existsSync(filePath) || !fs.statSync(filePath).isFile()){
             sendResponse(socket, "HTTP/1.1 404 Not Found\r\n\r\n");
             socket.end();
             return;
         }
         const file = fs.readFileSync(filePath);
         sendResponse(socket, `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${file.length}\r\n\r\n${file}`);
-    } else {
+    }else{
         sendResponse(socket, "HTTP/1.1 404 Not Found\r\n\r\n");
         socket.end();
     }
 };
 
 const handlePostRequest = (socket, path, requestData) => {
-    if (path.startsWith("/files/") && path.length > 7) {
+    if(path.startsWith("/files/") && path.length > 7){
         const fileName = directory + "/" + path.substring(7);
         const req = requestData.toString().split("\r\n");
         const body = req[req.length - 1];
         fs.writeFileSync(fileName, body);
         sendResponse(socket, "HTTP/1.1 201 Created\r\n\r\n");
-    } else {
+    }else{
         sendResponse(socket, "HTTP/1.1 404 Not Found\r\n\r\n");
         socket.end();
     }
@@ -77,7 +73,7 @@ const sendResponse = (socket, response) => {
 
 const logToFile = (message) => {
     fs.appendFile(logFileName, message + "\n", (error) => {
-        if (error) {
+        if(error){
             console.error("Error writing to log file:", error);
         }
     });
@@ -105,7 +101,7 @@ const startServer = () => {
     });
 };
 
-if (cluster.isMaster) {
+if(cluster.isMaster){
     console.log(`Master ${process.pid} is running`);
     cluster.on("exit", (worker, code, signal) => {
         console.log(`Worker ${worker.process.pid} died`);
@@ -113,10 +109,9 @@ if (cluster.isMaster) {
     });
 
     // Fork workers based on CPU cores
-    for (let i = 0; i < numCPUs; i++) {
+    for(let i=0; i<numCPUs; i++)
         cluster.fork();
-    }
-} else {
+}else{
     console.log(`Worker ${process.pid} started`);
     startServer();
 }
